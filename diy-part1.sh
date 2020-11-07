9a3be820564b695b3a27ee085477d30c6577d427
Li Xin <ibrikex1@gmail.com>
2020-11-07 17:36:05 +0800

    修正书写错误

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # add kenzo feed source
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git kenzo https://github.com/kenzok8/small.git
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/small.git' feeds.conf.default

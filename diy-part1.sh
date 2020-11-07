0f46c46fcf5ea03db8f52da82deb30624434063f
Li Xin <ibrikex1@gmail.com>
2020-11-07 18:32:08 +0800

    改正一处错误
    src-git的alias不能重名

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 
 # add kenzo feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
